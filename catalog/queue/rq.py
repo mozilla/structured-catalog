4c64e6ff6325299eed6230f1d4b6cb748e8da0e3
Andrew Halberstadt <halbersa@gmail.com>
2015-01-07 10:01:43 -0500

    Refactor rq imports

--- catalog/queue/rq.py
+++ catalog/queue/rq.py
@@ -1,13 +1,15 @@
 from .base import BaseQueue
 from ..worker import process_test_job
 
-redis = None
-rq = None
+Redis = None
+Connection = None
+Queue = None
 def do_delayed_imports():
-    global redis
-    global rq
-    import redis
-    import rq
+    global Redis
+    global Connection
+    global Queue
+    from redis import Redis
+    from rq import Queue, Connection
 
 class RQueue(BaseQueue):
 
@@ -15,11 +17,11 @@ class RQueue(BaseQueue):
         do_delayed_imports()
         BaseQueue.__init__(self)
 
-        self.redis_conn = redis.Redis()
-        self.queue = rq.Queue('catalog', connection=self.redis_conn)
+        with Connection(Redis()):
+            self.q = Queue('catalog')
 
     def push(self, job):
-        self.queue.enqueue(process_test_job, job)
+        self.q.enqueue(process_test_job, job)
 
     def get(self):
         # rq delivers jobs to workers automatically, no need for polling
